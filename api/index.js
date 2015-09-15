54198fe53ee9b20f9cfb340ed8711c6eb36514a5
Maks Nemisj <maks@nemisj.com>
2015-09-15 17:21:32 +0200

    allow all headers

--- api/index.js
+++ api/index.js
@@ -13,7 +13,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
 
   if (req.method.toLowerCase() == 'options') {
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
   }
   next();
 });
