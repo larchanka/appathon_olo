f678412f10278f73a0266f0c038dc9c95081d331
Maks Nemisj <maks@nemisj.com>
2015-09-15 17:12:39 +0200

    allow CORS cookies

--- api/index.js
+++ api/index.js
@@ -8,8 +8,9 @@ require('./db.js');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "http://localhost:9999");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie, Set-Cookie");
   next();
 });
 
